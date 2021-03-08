dea4373664bee01d4cd728de9cc99c36dd1de210
azzaare <jf@baffier.fr>
2021-03-08 16:51:18 +0900

    Added full doc for vector.jl

--- src/vector.jl
+++ src/vector.jl
@@ -1,14 +1,26 @@
 
+"""
+    VectorFold{T,V <: AbstractVector{T}}
+A folded vector structure that extends the methods of AbstractVector to a folded structure.
+"""
 struct VectorFold{T,V <: AbstractVector{T}} <: PatternFold{T,V}
     pattern::V
     gap::T
     folds::Int
 end
 
+"""
+    pattern(vf, index)
+Return the element at `index` in the original pattern.
+"""
 pattern(vf, index) = pattern(vf)[index]
 
 # REVIEW - how to optimize assignation
 # TODO - can unfold be made almost generic?
+"""
+    unfold(vf::VectorFold; from=1, to=folds(vf))
+Construct the unfolded version of `vf` (with the same type as `pattern(vf)`) based. Please note that using an iterator on `vf` avoid memory allocation, which is not the case of `unfold`.
+"""
 function unfold(vf::VectorFold; from=1, to=folds(vf))
     pl = pattern_length(vf)
     ul = (to - from + 1) * pl
