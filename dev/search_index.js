a20e7e364bc825da71b2f0c2a4019eaceb3ff4bd
Documenter.jl <documenter@juliadocs.github.io>
2021-03-08 04:27:38 +0000

    build based on 48bacda

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PatternFolds","category":"page"},{"location":"#PatternFolds","page":"Home","title":"PatternFolds","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PatternFolds]","category":"page"}]
+}
