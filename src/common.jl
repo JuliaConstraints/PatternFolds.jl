e885fdc8ac06a28a9cf8a5718a7e4f8863672cac
azzaare <jf@baffier.fr>
2021-03-08 16:17:59 +0900

    Basic doc for PatternFold interface

--- src/common.jl
+++ src/common.jl
@@ -1,3 +1,11 @@
+"""
+    PatternFold{T, P}
+An abstract stype used as an interface for folded patterns such as `VectorFold`.
+To implement the interface and inherit from it, a new structure must define three fields:
+- `pattern::P`. Note that both `length(::P)` and `rand(:P)` methods must be available
+- `gap::T`
+- `folds::int`
+"""
 abstract type PatternFold{T, P} end
 
 pattern(pf) = pf.pattern
