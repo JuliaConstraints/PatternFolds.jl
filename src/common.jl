4a9ee5730632260595d28fddaf2790753fdcfe1c
azzaare <jf@baffier.fr>
2021-03-08 16:36:19 +0900

    Added full doc for common.jl

--- src/common.jl
+++ src/common.jl
@@ -8,15 +8,41 @@ To implement the interface and inherit from it, a new structure must define thre
 """
 abstract type PatternFold{T, P} end
 
+"""
+    pattern(<:PatternFold)
+Return the *pattern* of any `PatternFold`. The pattern defines the occurences of the first fold.
+"""
 pattern(pf) = pf.pattern
+
+"""
+    gap(<:PatternFold)
+Return the *gap* between the starts of consecutive folds.
+"""
 gap(pf) = pf.gap
+
+"""
+    folds(<:PatternFold)
+Return the number of *folds*. An infinite folded pattern returns `0`.
+"""
 folds(pf) = pf.folds
 
 # TODO - look if another name is more appropriate
+"""
+    pattern_length(pf<:PatternFold)
+Return the length of the basic pattern of `pf`.
+"""
 pattern_length(pf) = length(pattern(pf))
 
+"""
+    length(pf<:PatternFold)
+Return the length of `pf` if unfolded.
+"""
 length(pf) = pattern_length(pf) * folds(pf)
 
+"""
+    rand(pf<:PatternFold)
+Returns a random value of `pf` as if it was unfolded.
+"""
 function rand(pf::PF) where {PF <: PatternFold}
     return rand(pattern(pf)) + rand(0:(folds(pf) - 1)) * gap(pf)
 end
