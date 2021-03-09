bb8182261de0b89144389a7cfae7ad7f726859cd
Azzaare <jf@baffier.fr>
2021-03-09 14:43:11 +0900

    Added Mutable VectorFold

--- src/PatternFolds.jl
+++ src/PatternFolds.jl
@@ -3,12 +3,13 @@ module PatternFolds
 import Base.length, Base.rand, Base.iterate
 
 # exports
-export PatternFold, VectorFold
+export PatternFold, VectorFold, MVectorFold
 export pattern, gap, folds
 export length, unfold
 
 # includes
 include("common.jl")
 include("vector.jl")
+include("mutable.jl")
 
 end
