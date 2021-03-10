2f903808837942f3224ce0b9c1f5890e993ed0c0
azzaare <jf@baffier.fr>
2021-03-10 15:10:34 +0900

    Some progress on iterate

--- src/intervals.jl
+++ src/intervals.jl
@@ -2,12 +2,25 @@
 struct Interval{T <: Real}
     a::Tuple{T, Bool}
     b::Tuple{T, Bool}
-end
 
-const Intervals{T} = Vector{Interval{T}}
+    function Interval{T}(a, b) where {T <: Real}
+        bounds = (a[1] < b[1] ? (a, b) : (b, a))
+        return new{T}(bounds[1], bounds[2])
+    end
+end
+Interval(a, b) = Interval{typeof(a[1])}(a,b)
 
 a(i) = i.a
 b(i) = i.b
++(bound::Tuple{T, Bool}, gap::T) where T = bound[1]+gap, bound[2]
++(i::Interval, gap) = Interval(a(i) + gap, b(i) + gap)
+
+function Base.in(value, i::Interval)
+    x, y = value(i, :a), value(i, :b)
+    lesser = closed(i, :a) ? x ≤ value : x < value
+    greater = closed(i, :b) ? x ≥ value : x > value
+    return lesser && greater
+end
 
 value(i, ::Val{:a}) = i.a[1]
 value(i, ::Val{:b}) = i.b[1]
@@ -18,8 +31,6 @@ closed(i, ::Val{:b}) = i.b[2]
 closed(i, bound) = closed(i, Val(bound))
 opened(i, bound) = !closed(i, bound)
 
-
-
 Base.length(i::Interval) = value(i, :b) - value(i, :a)
 Base.isempty(i::Interval) = lenght(i) == 0 && (opened(i, :a) || opened(i, :b))
 Base.ndims(::Interval) = 1
@@ -29,15 +40,49 @@ mutable struct IntervalsFold{T <: Real} <: PatternFold{T, Interval{T}}
     pattern::Interval{T}
     gap::T
     folds::Int
+    current::Int
 end
 
-@forward IntervalsFold.pattern a, b
+IntervalsFold(p, g, f, c = 1) = IntervalsFold(p, g, f, c)
 
-+(bound::Tuple{T, Bool}, gap::T) where T = bound[1]+gap, bound[2]
+@forward IntervalsFold.pattern a, b
 
 function unfold(isf::IntervalsFold)
     x, y = a(isf), b(isf)
     g = gap(isf)
     f = folds(isf)
     return map(i -> Interval(x + g * i, y + g * i), 0:(f - 1))
+end
+
+function set_fold!(isf::IntervalsFold, new_fold = isf.current + 1)
+    if new_fold != isf.current && 0 < new_fold ≤ isf.folds
+        distortion = gap(isf) * (new_fold - isf.current)
+        isf.pattern += distortion
+        isf.current = new_fold
+    end
+end 
+
+function Base.iterate(iter::IntervalsFold)
+    reset_pattern!(iter)
+    return pattern(iter), 1
+end
+
+# "Induction" iterate method
+function Base.iterate(iter::IntervalsFold, state::Int)
+    state ≥ folds(iter) && return nothing
+    set_fold!(iter, state)
+	next_state = state + 1
+	elem = pattern(iter)
+	return elem, next_state
+end
+
+# Reverse iterate method
+function Base.iterate(r_iter::Base.Iterators.Reverse{IntervalsFold{T}},
+    state::Int = length(r_iter.itr)) where {T}
+	state < 1 && return nothing
+	next_state = state - 1
+	iter = r_iter.itr
+	elem = pattern(iter)
+    set_fold!(iter, next_state)
+	return elem, next_state
 end
