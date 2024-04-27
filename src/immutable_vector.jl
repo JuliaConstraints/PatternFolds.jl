5a409dbdc4ab4b790b6f162c6da6f70921d2a287
Jean-Francois Baffier <jf@baffier.fr>
2024-04-27 16:18:03 +0900

    Update CI, format, tests, doc, etc. (#36)
    * Auto sciml format with JuliaFormatter (#34)
    
    * Update CIs (#35)
    
    * Update compat helper
    
    * Update compat helper 2
    
    * Update tag bot
    
    * Add format (and format yml files
    
    * Add a spell check action
    
    * Update CI to include format
    
    * Update CI to include format 2
    
    * Fix a typo from the new github action
    
    * Update CI
    
    * Adds Aqua.jl
    
    * Fix Aqua errors (but 2 to broken tests)
    
    * Fix format

--- src/immutable_vector.jl
+++ src/immutable_vector.jl
@@ -2,7 +2,7 @@
     VectorFold{T,V <: AbstractVector{T}}
 A folded vector structure that extends the methods of AbstractVector to a folded structure.
 """
-struct IVectorFold{T,V <: AbstractVector{T}} <: AbstractVectorFold{T}
+struct IVectorFold{T, V <: AbstractVector{T}} <: AbstractVectorFold{T}
     pattern::V
     gap::T
     folds::Int
@@ -20,7 +20,7 @@ pattern(vf, index) = pattern(vf)[index]
     unfold(vf::VectorFold; from=1, to=folds(vf))
 Construct the unfolded version of `vf` (with the same type as `pattern(vf)`) based. Please note that using an iterator on `vf` avoid memory allocation, which is not the case of `unfold`.
 """
-function unfold(vf::IVectorFold; from=1, to=folds(vf))
+function unfold(vf::IVectorFold; from = 1, to = folds(vf))
     pl = pattern_length(vf)
     ul = (to - from + 1) * pl
     v = typeof(pattern(vf))(undef, ul)
@@ -45,38 +45,38 @@ Base.iterate(iter::IVectorFold) = (pattern(iter, 1), 1) # Base case iterate meth
 function Base.iterate(iter::IVectorFold, state::Int)
     state ≥ length(iter) && return nothing
 
-	next_state = state + 1
+    next_state = state + 1
     pl = pattern_length(iter)
 
-	pattern_counter = mod1(next_state, pl)
-	fold_counter = state ÷ pl
-	elem = pattern(iter, pattern_counter) + (fold_counter * gap(iter))
+    pattern_counter = mod1(next_state, pl)
+    fold_counter = state ÷ pl
+    elem = pattern(iter, pattern_counter) + (fold_counter * gap(iter))
 
-	return elem, next_state
+    return elem, next_state
 end
 
 # Reverse iterate method
-function Base.iterate(r_iter::Base.Iterators.Reverse{IVectorFold{T,V}},
-    state::Int = length(r_iter.itr)
-) where {T,V}
-	state < 1 && return nothing
+function Base.iterate(r_iter::Base.Iterators.Reverse{IVectorFold{T, V}},
+        state::Int = length(r_iter.itr)
+) where {T, V}
+    state < 1 && return nothing
 
-	next_state = state - 1
-	iter = r_iter.itr
+    next_state = state - 1
+    iter = r_iter.itr
     pl = pattern_length(iter)
 
-	pattern_counter = mod1(state, pl)
-	fold_counter = next_state ÷ pl
-	elem = pattern(iter, pattern_counter) + (fold_counter * gap(iter))
+    pattern_counter = mod1(state, pl)
+    fold_counter = next_state ÷ pl
+    elem = pattern(iter, pattern_counter) + (fold_counter * gap(iter))
 
-	return elem, next_state
+    return elem, next_state
 end
 
 # Folding a vector to give a suitable VectorFold
 check_pattern(v, w, gap) = all(i -> i == gap, w - v)
 
 function check_pattern(v, i, gap, fold)
-    for j in 1:(fold -1)
+    for j in 1:(fold - 1)
         v_start, v_end = (j - 1) * i + 1, j * i
         w_start, w_end = j * i + 1, (j + 1) * i
         !check_pattern(v[v_start:v_end], v[w_start:w_end], gap) && return false
--- src/vector.jl
+++ src/vector.jl
@@ -3,14 +3,14 @@
 A mutable structure for folded vector that extends the methods of AbstractVector. Compared to `IVectorFold`, this tructure is about 20% faster using iterators.
 Note that this structure keep an active pointer to the `current` *unfolded* pattern. However, its external behavior is similar to `IVectorFold`.
 """
-mutable struct VectorFold{T,V<:AbstractVector{T}} <: AbstractVectorFold{T}
+mutable struct VectorFold{T, V <: AbstractVector{T}} <: AbstractVectorFold{T}
     pattern::V
     gap::T
     folds::Int
     current::Int
 end
 
-VectorFold(p, g, f; c=1) = VectorFold(p, g, f, c)
+VectorFold(p, g, f; c = 1) = VectorFold(p, g, f, c)
 
 pattern_length(mvf::VectorFold) = length(mvf.pattern)
 
@@ -25,7 +25,7 @@ pattern(mvf::VectorFold, index) = pattern(mvf)[index]
     set_fold!(mvf::VectorFold, new_fold = mvf.current + 1)
 Set the *unfolded* pattern to `new_fold`. By default move the next *fold* after `current`.
 """
-function set_fold!(mvf, new_fold=mvf.current + 1)
+function set_fold!(mvf, new_fold = mvf.current + 1)
     if new_fold != mvf.current && 0 < new_fold ≤ mvf.folds
         distortion = gap(mvf) * (new_fold - mvf.current)
         mvf.pattern .+= distortion
@@ -55,8 +55,8 @@ end
 
 # Reverse iterate method
 function Base.iterate(
-    r_iter::Base.Iterators.Reverse{VectorFold{T,V}}, state::Int=length(r_iter.itr)
-) where {T,V}
+        r_iter::Base.Iterators.Reverse{VectorFold{T, V}}, state::Int = length(r_iter.itr)
+) where {T, V}
     state < 1 && return nothing
 
     iter = r_iter.itr
@@ -81,7 +81,7 @@ end
 Base.rand(mvf::VectorFold, n::Int) = map(_ -> rand(mvf), 1:n)
 
 # Specific dispatch for MVectorFold
-function unfold(mvf::VectorFold; from=1, to=folds(mvf))
+function unfold(mvf::VectorFold; from = 1, to = folds(mvf))
     pl = pattern_length(mvf)
     ul = (to - from + 1) * pl
     v = typeof(mvf.pattern)(undef, ul)
@@ -101,22 +101,22 @@ make_vector_fold(pattern, gap, fold, ::Val{:mutable}) = VectorFold(pattern, gap,
 
 @testitem "VectorFold" tags=[:vectors] begin
     vf_dict = Dict([
-        make_vector_fold([1,2], 10, 5, :immutable) => Dict(
-            :pattern => [1,2],
+        make_vector_fold([1, 2], 10, 5, :immutable) => Dict(
+            :pattern => [1, 2],
             :gap => 10,
             :folds => 5,
             :length => 10,
-            :unfold => [1,2,11,12,21,22,31,32,41,42],
-            :reverse => reverse([1,2,11,12,21,22,31,32,41,42]),
+            :unfold => [1, 2, 11, 12, 21, 22, 31, 32, 41, 42],
+            :reverse => reverse([1, 2, 11, 12, 21, 22, 31, 32, 41, 42])
         ),
-        make_vector_fold([1,2], 10, 5, :mutable) => Dict(
-            :pattern => [1,2],
+        make_vector_fold([1, 2], 10, 5, :mutable) => Dict(
+            :pattern => [1, 2],
             :gap => 10,
             :folds => 5,
             :length => 10,
-            :unfold => [1,2,11,12,21,22,31,32,41,42],
-            :reverse => reverse([1,2,11,12,21,22,31,32,41,42]),
-        ),
+            :unfold => [1, 2, 11, 12, 21, 22, 31, 32, 41, 42],
+            :reverse => reverse([1, 2, 11, 12, 21, 22, 31, 32, 41, 42])
+        )
     ])
 
     for (vf, results) in vf_dict
@@ -131,22 +131,22 @@ make_vector_fold(pattern, gap, fold, ::Val{:mutable}) = VectorFold(pattern, gap,
         @test collect(Iterators.reverse(vf)) == results[:reverse]
         @test reverse(collect(vf)) == results[:reverse]
     end
-    @test isempty(make_vector_fold(Vector(),1,1,:immutable))
-    @test isempty(make_vector_fold(Vector(),1,1))
+    @test isempty(make_vector_fold(Vector(), 1, 1, :immutable))
+    @test isempty(make_vector_fold(Vector(), 1, 1))
 
-    v1 = make_vector_fold([42,3,45,6],13,4)
+    v1 = make_vector_fold([42, 3, 45, 6], 13, 4)
     w1 = unfold(v1)
     v11 = fold(w1)
 
     @test unfold(v11) == w1
 
-    v2 = make_vector_fold([34,34,43,43],10,3)
+    v2 = make_vector_fold([34, 34, 43, 43], 10, 3)
     w2 = unfold(v2)
     v22 = fold(w2)
 
     @test unfold(v22) == w2
 
-    v3 = make_vector_fold([42,3,45,6],13,4,:immutable)
+    v3 = make_vector_fold([42, 3, 45, 6], 13, 4, :immutable)
     w3 = unfold(v3)
     v33 = fold(w3)
 
